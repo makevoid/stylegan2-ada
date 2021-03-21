3774a2da49dba8210893288983ae6ffdf926e0aa
Robert A. Gonsalves <robgonsalves@gmail.com>
2021-03-21 16:59:11 -0400

    Allow p to be set when using ADA

--- train.py
+++ train.py
@@ -246,7 +246,7 @@ def setup_training_options(
 
     if p is not None:
         assert isinstance(p, float)
-        if aug != 'fixed':
+        if aug != 'fixed' and aug != 'ada':
             raise UserError('--p can only be specified with --aug=fixed')
         if not 0 <= p <= 1:
             raise UserError('--p must be between 0 and 1')
